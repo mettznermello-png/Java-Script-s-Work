eee81126d55c6d1287ea9f2d68a7bd835b1a862a
João Mello <mettznermello@gmail.com>
2026-02-23 21:28:48 -0300

    renanponick tasks

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// Não usar ESPAÇO. ACENTO, Ç, @$!%$¨
+
+//number é uma fução que tenta converter um valor passa para numero
+const numero = Number(prompt("Digite um número:"))
+const outronumero = Number(prompt("Digite outro Número:"))
+
+alert(numero+outronumero)
+
+// colocar o + entre alguma coisa
