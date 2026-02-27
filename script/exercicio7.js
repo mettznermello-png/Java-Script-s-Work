dd1d2c6c775c7a20289431902ef33f104bceb9f7
João Mello <mettznermello@gmail.com>
2026-02-26 21:45:29 -0300

    é isso

--- script/exercicio7.js
+++ script/exercicio7.js
@@ -4,5 +4,5 @@ const distância = Number(prompt("Qual é a distancia em Km?"))
 const resultado1 = distância*1000
 const resultado2 = distância*100000
 //saída
-alert(resultado1)
-alert(resultado2)
+alert(resultado1 + "m")
+alert(resultado2 + "cm")
