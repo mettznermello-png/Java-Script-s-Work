7c83cdd61fd2fe554f59f985c90120229f1b8517
João Mello <mettznermello@gmail.com>
2026-02-26 21:35:12 -0300

    exercicio6

---
+++ script/exercicio6.js
@@ -0,0 +1,10 @@
+//entrada
+const tempo = Number(prompt("Por quanto tempo você esquenta seu café?"))
+//processamento
+const resultado1 = tempo*3600
+const resultado2 = tempo*60
+const resultado3 = tempo
+//saida
+alert(resultado1 + "horas")
+alert(resultado2 + "minutos")
+alert(resultado3 + "segundos")
