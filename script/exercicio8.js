dcf2c181ce6eb1f722b3c6c0860eef872f07a4f3
João Mello <mettznermello@gmail.com>
2026-02-26 21:53:05 -0300

    exercicio8.js

---
+++ script/exercicio8.js
@@ -0,0 +1,24 @@
+//entrada
+const número = Number(prompt("Digite qualquer número:"))
+//processamento
+const resultado1 = número*1
+const resultado2 = número*2
+const resultado3 = número*3
+const resultado4 = número*4
+const resultado5 = número*5
+const resultado6 = número*6
+const resultado7 = número*7
+const resultado8 = número*8
+const resultado9 = número*9
+const resultado10 = número*10
+//saída
+alert(resultado1)
+alert(resultado2)
+alert(resultado3)
+alert(resultado4)
+alert(resultado5)
+alert(resultado6)
+alert(resultado7)
+alert(resultado8)
+alert(resultado9)
+alert(resultado10)
