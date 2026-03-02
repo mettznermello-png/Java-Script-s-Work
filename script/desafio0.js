0145e4204c8c3e5e2d8033ec11db318cebbcb2ba
João Mello <mettznermello@gmail.com>
2026-03-02 19:19:24 -0300

    desafio0 ajustado

--- script/desafio0.js
+++ script/desafio0.js
@@ -1,5 +1,5 @@
 const idade = Number(prompt("Qual é a sua idade?"))
 const resultado1 = idade*12
 const resultado2 = idade*365
-alert(resultado1)
-alert(resultado2)
+alert(resultado1 + "meses")
+alert(resultado2 + "dias")
