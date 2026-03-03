cb4ac9925ef5a70fcca7c1abd9d842c16a8cb40f
João Mello <mettznermello@gmail.com>
2026-03-02 21:01:09 -0300

    exemplo

--- script/exemplo.js
+++ script/exemplo.js
@@ -1,2 +1,6 @@
-const footer = document.getElementById("footer")
-footer.style.color = "blue"
+//const footer = document.getElementById("footer")
+//footer.style.color = "blue"
+
+const novoTitulo = document.createElement("h2")
+novoTitulo.textContentContent = "Um novo Título"
+console.log(novoTitulo)
