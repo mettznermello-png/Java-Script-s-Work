693e85b61556ca489fc1a63c1641da416d5b4c55
João Mello <mettznermello@gmail.com>
2026-03-02 22:12:25 -0300

    commit  the new change (desafio1)

---
+++ script/desafio1.js
@@ -0,0 +1,4 @@
+const número = Number(pormpt("Digite um número:"))
+const outroNúmero = Number(prompt("Digite outro Numero:"))
+alert(número)
+alert(outroNúmero)
--- script/exemplo.js
+++ script/exemplo.js
@@ -1,6 +1,21 @@
-//const footer = document.getElementById("footer")
+const footer = document.getElementById("footer")
 //footer.style.color = "blue"
 
 const novoTitulo = document.createElement("h2")
-novoTitulo.textContent = "Um novo Título"
-console.log(novoTitulo)
+novoTitulo.textContent = "Um Novo Titulo"
+//console.log(novoTitulo)
+
+footer.appendChild(novoTitulo)
+// getElementById = puxa um elemento do HTML
+
+const header = document.getElementById("exemplo")
+header.appendChild(novoTitulo)
+
+
+//const button = document.getElementById('my-button');
+//button.addEventListener('click', () => {
+//    alert('button was clicked');
+//});
+header.addEventListener("click", () => {
+    alert("it's going down now, just let's us adore you")
+})
