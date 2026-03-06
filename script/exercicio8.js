8b3218ef0204fb55785f9e5d05e64c9885d67262
João Mello <mettznermello@gmail.com>
2026-03-05 22:16:22 -0300

    8 not finished

--- script/exercicio8.js
+++ script/exercicio8.js
@@ -1,6 +1,5 @@
-//entrada
-const número = Number(prompt("Digite qualquer número:"))
-//processamento
+function exercicio8() {
+    const número = Number(prompt("Digite qualquer número:"))
 const resultado1 = número*1
 const resultado2 = número*2
 const resultado3 = número*3
@@ -11,7 +10,6 @@ const resultado7 = número*7
 const resultado8 = número*8
 const resultado9 = número*9
 const resultado10 = número*10
-//saída
 alert(resultado1)
 alert(resultado2)
 alert(resultado3)
@@ -22,3 +20,6 @@ alert(resultado7)
 alert(resultado8)
 alert(resultado9)
 alert(resultado10)
+}
+const buttonExercicio8 = document.getElementById("exercicio8")
+buttonExercicio8.addEventListener('click', () => )
