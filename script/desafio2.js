9d56f1b0695e7d36a4b3dbc5a91efe01a18b4431
João Mello <mettznermello@gmail.com>
2026-03-12 22:20:39 -0300

    mello

---
+++ script/desafio2.js
@@ -0,0 +1,6 @@
+const A = Number(prompt("Digite um valor:"))
+const B = Number(prompt("digite outro valor"))
+if ( A < B ) {
+    alert("A é menor que B")
+} else 
+    alert("B é maior que A")
--- script/exemplo-if.js
+++ script/exemplo-if.js
@@ -1,6 +1,16 @@
-const numero = Number(prompt("digite um numero"))
-if (numero < 10){
-    alert("menor que 10")
+const nota1 = Number(prompt("digite sua primeira nota:"))
+const nota2 = Number(prompt("digite sua segunda nota:"))
+const nota3 = Number(prompt("digite sua terceira nota:"))
+
+const media = (nota1+nota2+nota3)/3
+//console.log(media)
+//console está no f12 na aba console
+if (media < 3 ) {
+    alert("Melhore (é sério)")
+} else if ( media >= 3 && media <= 7 ) {
+    alert("Dá pra melhorar")
+} else if (media > 7 && media <= 10) {
+    alert("Exelente, merece uma balinha")
 } else {
-    alert("maior que 9")
+    alert("Essa nota não existe")
 }
